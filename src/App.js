69f682f3b3cbd4526a8fa539c1186a1e19a45f6d
tatitokyo <hompodoeva.tatiana@gmail.com>
2023-10-16 12:23:12 +0900

    роутер

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { Header } from "./components/header";
 import { MainSection } from "./components/main-section/main-section";
 import { Wrapper } from "./components/wrapper";
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from '../src/pages/login';
+import RegisterPage from '../src/pages/register';
 
 function App() {
   return (
-    <Wrapper>
-      <Header />
-      <MainSection />
-    </Wrapper>
+    <div className="App">
+      <Routes>
+       <Route path="/login" element={<LoginPage />} />
+       <Route path="/register" element={<RegisterPage />} />
+       <Route path="/" element={
+        <Wrapper>
+          <Header />
+          <MainSection />
+        </Wrapper>} />
+      </Routes>
+    </div>
   );
 }
 
